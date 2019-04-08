9269d6c1f52090653bc75370f0bf0b0d24683c8c
Dan Kagan <dankagan7@gmail.com>
2019-04-08 20:48:22 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,9 +1,8 @@
 def second_challenge
   epic_tragedy = {
     :montague => {:patriarch => {}, :matriarch => {}, :hero => {}, :hero_friends => []}, 
-    :capulet => {
-      #your key/value pairs here!
-      }}
+    :capulet => {:patriarch => {}, :matriarch => {}, :heroine => {}, :heroine_friends => []}
+  }
 
   
 end
