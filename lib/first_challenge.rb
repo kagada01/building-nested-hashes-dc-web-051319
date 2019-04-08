29145708c3bb7748200eb5246c4dfdafb1f80ae5
Dan Kagan <dankagan7@gmail.com>
2019-04-08 20:41:34 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -5,7 +5,11 @@ def first_challenge
         :name => "Lord Montague"
         :age => "53"
       },
-      :matriarch => {},
+      :matriarch => {
+        :name => "Lady Montague"
+        :age => "54"
+        
+      },
       :hero => {},
       :hero_friends => []
     },
